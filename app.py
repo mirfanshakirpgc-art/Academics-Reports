84d8e0abf052a2336865300b3b945d7ec1456b27
mirfanshakirpgc-art <mirfanshakirpgc@gmail.com>
2026-05-29 21:38:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ if st.session_state.logged_in:
    if st.session_state.logged_in:  # LINE 130 (LEAVE ALONE)
     col1, col2 = st.columns([1, 8])
     with col1:
-        st.image("logo.jpg", width=75)
+        st.image("logo.jpg", width=90)
     with col2:
         st.title("Concordia Colleges, Kasur")
     try:
