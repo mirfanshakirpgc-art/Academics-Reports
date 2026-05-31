8d487448ca4ef3d2bafb6a3b3b0af6480c5e03ba
mirfanshakirpgc-art <mirfanshakirpgc@gmail.com>
2026-05-31 13:00:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ elif menu_choice == "🪪 Student Result Cards":
                 subjects_list = DISCIPLINE_SUBJECTS_MAP[matched_disp]
                 raw_marks = run_query("SELECT UPPER(TRIM(subject)) as subject, TRIM(exam_type) as exam_type, marks_obtained, total_marks FROM marks WHERE student_id = :id", {"id": current_id})
                 
-               logo_base64 = "https://raw.githubusercontent.com/mirfanshakirpgc-art/Academics-Reports/main/logo.png"
+logo_base64 = "https://raw.githubusercontent.com/mirfanshakirpgc-art/Academics-Reports/main/logo.png"
 
                 compiled_html += f"""
                 <div class="official-card-container">
